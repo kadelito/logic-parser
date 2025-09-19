5d30011ecc2ec77a7f77b46ed4c70a67fb579639
kadelito <malayabarenio@gmail.com>
2025-09-19 14:31:14 -0400

    Improved documentation

--- src/main/java/common/PropositionEntry.java
+++ src/main/java/common/PropositionEntry.java
@@ -5,7 +5,12 @@ import common.propositions.Proposition;
 
 import java.util.Set;
 
+/**
+ * A record holding a proposition and a set of its related atomic propositions.
+ * @param proposition
+ * @param atomics
+ */
 public record PropositionEntry(
-        Proposition value,
+        Proposition proposition,
         Set<AtomicProposition> atomics
 ) {}
--- src/main/java/common/propositions/Proposition.java
+++ src/main/java/common/propositions/Proposition.java
@@ -1,22 +1,27 @@
 package common.propositions;
 
+/**
+ * An abstract class representing a proposition.
+ *
+ * @see #evaluate()
+ */
 public abstract class Proposition {
     public static final AtomicProposition TRUE = new AtomicProposition("T", true, true);
     public static final AtomicProposition FALSE = new AtomicProposition("F", false, true);
 
     /**
-     * Returns the truth value of this proposition.
+     * Returns the truth proposition of this proposition.
      * <p>
-     * Implementations should call this method on child propositions and use the resulting value.
+     * Implementations should call this method on child propositions (if any) and use the resulting proposition.
      *
-     * @return the truth value of this proposition
+     * @return the truth proposition of this proposition
      */
     public abstract boolean evaluate();
 
     /**
      * Returns a string representation of this proposition.
      * <p>
-     * Implementations should include the value of repr when called on child propositions.
+     * Implementations should include the proposition of repr when called on child propositions.
      *
      * @return the complete representation
      */
