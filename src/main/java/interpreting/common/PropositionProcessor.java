84872bf7af2064f39b925dcacc5a0cfc52b90781
kadelito <malayabarenio@gmail.com>
2025-09-20 21:11:36 -0400

    Moved PropositionProcessor.java from interpreting.parsing to interpreting.common

---
+++ src/main/java/interpreting/common/PropositionProcessor.java
@@ -0,0 +1,69 @@
+package interpreting.common;
+
+import common.PropositionEntry;
+import common.propositions.AtomicProposition;
+import interpreting.parsing.Parser;
+import interpreting.tokenization.Lexer;
+import logic.LogicContext;
+
+/**
+ *
+ * After successfully generating a tree, any {@link AtomicProposition AtomicPropositions} are saved to ensure shared instances.
+ * <p>
+ * For example, if a parser generates a tree represented by <code>"p ^ q"</code> then generates <code>"q -> r"</code>
+ * without modifying context externally or through {@link #setContext}
+ * the <code>AtomicProposition</code> with representation <code>"q"</code> will be the same instance among both propositions.
+ * @see LogicContext
+ * @see AtomicProposition
+ */
+public class PropositionProcessor {
+    private Lexer lexer;
+    private Parser parser;
+    private LogicContext context;
+    private InterpretingResult<PropositionEntry> prevTreeResult;
+
+    public PropositionProcessor(String input, LogicContext context) {
+        this.context = context;
+        this.lexer = new Lexer(input);
+        this.parser = new Parser(lexer, context);
+    }
+
+    public PropositionProcessor(String input) {
+        this(input, new LogicContext());
+    }
+
+    public PropositionProcessor(LogicContext context) {
+        this("", context);
+    }
+
+    public PropositionProcessor() {
+        this("", new LogicContext());
+    }
+
+    public void generateProposition() {
+        prevTreeResult = parser.buildPropositionTree();
+    }
+
+    public void generateProposition(String newStr) {
+        setInput(newStr);
+        generateProposition();
+    }
+
+    public boolean generateSucceeded() {return prevTreeResult.value() != null;}
+
+    public PropositionEntry getLastProposition() {
+        return prevTreeResult.value();
+    }
+
+    public String getErrorMessage() {
+        return prevTreeResult.message();
+    }
+
+    public void clearContext() {parser.clearContext();}
+
+    public void setInput(String input) {
+        lexer.setInput(input);
+    }
+
+    public void setContext(LogicContext context) {parser.setContext(context);}
+}
