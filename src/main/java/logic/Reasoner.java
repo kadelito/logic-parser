bfc5f705377b12d85b206383e56c31adb0215cee
kadelito <malayabarenio@gmail.com>
2025-09-21 14:45:08 -0400

    Cleaned up javadoc in Reasoner

--- src/main/java/logic/Reasoner.java
+++ src/main/java/logic/Reasoner.java
@@ -5,17 +5,28 @@ import common.PropositionEntry;
 import java.util.Collection;
 
 /**
- * The interface Reasoner.
+ * An interface allowing for different implementations of two key operations:
+ * <ul>
+ *     <li>Checking for equivalence between two {@link PropositionEntry propositions} with {@link #areEqual}, and</li>
+ *     <li>Evaluating validity of an argument with {@link #isArgumentValid}.</li>
+ * </ul>
+ * These methods should return a consistent value regardless
+ * of the values of each {@link common.propositions.AtomicProposition AtomicProposition}
+ * associated with each {@link PropositionEntry}.
+ * <p>
+ * After calling these methods, the value of each <code>AtomicProposition</code> may be different
+ * from what they originally were.
  */
 public interface Reasoner {
     /**
      * Determines whether, for all combinations of truth values
-     * for their {@link common.propositions.AtomicProposition AtomicProposition}s,
-     * two {@link common.PropositionEntry Proposition}s have equivalent truth values.
+     * for their {@link common.propositions.AtomicProposition AtomicPropositions},
+     * two {@link common.PropositionEntry Propositions} have equivalent truth values.
      * <p>
-     * Both propositions should be in the same context, which means any AtomicPropositions with identical names should share an instance.
+     * Both propositions should be in the same context,
+     * which means any <code>AtomicPropositions</code> with identical names should share an instance.
      * <p>
-     * A return value of null signifies something went wrong.
+     * A return value of <code>null</code> signifies something went wrong during the process.
      *
      * @param p1 the first proposition
      * @param p2 the second proposition
@@ -31,7 +42,7 @@ public interface Reasoner {
      * All premises and the conclusion should be in the same context,
      * which means any AtomicPropositions with identical names should share an instance.
      * <p>
-     * A return value of null signifies something went wrong.
+     * A return value of <code>null</code> signifies something went wrong during the process.
      *
      * @param conclusion the conclusion to be proved
      * @param premises   the premises used to prove the conclusion
